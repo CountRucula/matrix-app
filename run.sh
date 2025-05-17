3ef16b1a482d83686e1bae6fb940160150f4555c
CountRucula <jannick.alder@gmx.ch>
2025-05-17 17:45:31 +0200

    add run script

---
+++ run.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Name of the branch to check
+branch="main"
+
+# Check network connectivity
+if ping -c 1 google.com &> /dev/null
+then
+  echo "Network is accessible."
+    # Fetch updates from the remote repository
+    git fetch
+
+    # Compare the local and remote branches
+    LOCAL=$(git rev-parse @)
+    REMOTE=$(git rev-parse @{u})
+
+    if [ $LOCAL != $REMOTE ]; then
+        echo "There are changes available to pull."
+
+        # Show the commit message from the available pull
+        echo "Commit message from the available pull:"
+        git log --oneline $LOCAL..$REMOTE
+
+        sleep 5
+
+        # Pull the changes
+        git pull origin $branch
+
+        # Restart the script
+        echo "Restarting the script..."
+        sh $0
+
+    else
+        echo "No changes available to pull."
+    fi
+else
+  echo "Network is not accessible."
+fi
+
+echo "Starting the application..."
+sleep 2
+export DISPLAY=:0
+venv/bin/python3 main.py
